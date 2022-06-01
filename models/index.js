a2d9c59775d9c0d05f57fc0770dcd0ce2ba26267
Keith <keithvip@gmail.com>
2022-06-01 13:38:35 -0500

    models

--- models/index.js
+++ models/index.js
@@ -3,5 +3,57 @@ const Party = require('./Party');
 const Statistics = require('./Statistics');
 const User = require('./User');
 
+// create associations
+User.hasMany(Character, {
+    foreignKey: 'user_id'
+});
+
+Character.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Character, {
+    through: Party,
+    as: 'party_id',
+    foreignKey: 'user_id'
+});
+
+Character.belongsToMany(User, {
+    through: Party,
+    as: 'party_id',
+    foreignKey: 'character_id'
+});
+
+Party.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Party.belongsTo(Character, {
+    foreignKey: 'character_id'
+});
+
+User.hasMany(Party, {
+    foreignKey: 'user_id'
+});
+
+Character.hasMany(Party, {
+    foreignKey: 'character_id'
+});
+
+Statistics.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Statistics.belongsTo(User, {
+    foreignKey: 'character_id'
+});
+
+User.hasMany(Statistics, {
+    foreignKey: 'user_id'
+});
+
+Character.hasMany(Statistics, {
+    foreignKey: 'character_id'
+});
 
 module.exports = { Character, Party, Statistics, User };
