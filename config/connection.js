09d53b4f99c2b93c8558166f962469e93462e009
Keith <keithvip@gmail.com>
2022-06-02 19:15:03 -0500

    working on seeds

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,15 @@
-const sequelize = require("sequelize");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-require("dotenv").config();
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3001,
+    }
+);
 
-let sequelize;
+module.exports = sequelize;
