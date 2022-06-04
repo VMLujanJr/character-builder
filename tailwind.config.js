90d99e6903d5cba0df476d5ab39dc7762e7c46d9
VMLujanJr <vmlujanjr@outlook.com>
2022-06-03 22:50:13 -0500

    initial homepage css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      dropShadow1: {
+        '3xl': '0 35px 35px rgba(255, 31, 47, 0.50)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
     },
   },
   plugins: [],
